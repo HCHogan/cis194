608f02ded65fa699eb42db2348e345ff0d86a3bf
Hank <hank@HankdeMac-Pro.local>
2024-05-31 06:04:06 -0700

    update

--- src/Hw5.hs
+++ src/Hw5.hs
@@ -1,21 +1,73 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Hw5 () where
 
-import Parser (parseExp)
-
-data ExprT
-  = Lit Integer
-  | Add ExprT ExprT
-  | Mul ExprT ExprT
-  deriving (Show, Eq)
+import Data.Maybe
+import ExprT
+import Parser
+import StackVM
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit n) = n
-eval (Add l r) = eval l + eval r
-eval (Mul l r) = eval l * eval r
+eval (ExprT.Add l r) = eval l + eval r
+eval (ExprT.Mul l r) = eval l * eval r
 
 -- Exercise 2
 evalStr :: String -> Maybe Integer
-evalStr input = case parseExp Lit Add Mul input of
+evalStr input = case parseExp Lit ExprT.Add ExprT.Mul input of
   Just expr -> Just (eval expr)
   Nothing -> Nothing
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit x
+    | x <= 0 = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 $ mod (x + y) 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ mod (x * y) 7
+
+testExp :: (Expr a) => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
+instance Expr StackVM.Program where
+  lit i = [StackVM.PushI i]
+  add a b = a ++ b ++ [StackVM.Add]
+  mul a b = a ++ b ++ [StackVM.Mul]
+
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
